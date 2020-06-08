45e70a7c213826551b418f5880a4a8e96f7b5882
oloyede <oloyedeoladimeji@hotmail.com>
2020-06-08 13:58:04 +0100

    patient and patientservice model up

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import enum
 from datetime import datetime
 
 from app import database
@@ -13,3 +14,36 @@ class User(database.Model):
 
     def __repr__(self):
         return '<User: {}>'.format(self.name)
+
+
+class HmoEnumProvider(enum.Enum):
+    HM01 = 'HM01'
+    HM02 = 'HM02'
+    HM03 = 'HM03'
+    HM04 = 'HM04'
+
+
+class Patient(database.Model):
+    __tablename__ = 'patient'
+    id = database.Column(database.Integer, autoincrement=True, primary_key=True)
+    name = database.Column(database.String(100), nullable=False, unique=True)
+    age = database.Column(database.Integer, nullable=False)
+    gender = database.Column(database.Boolean, nullable=False)
+    diagnosis = database.Column(database.String(255), nullable=False)
+    hmo_provider = database.Column(
+        database.Enum(HmoEnumProvider), nullable=False
+    )
+    patient_services = database.relationship('PatientService', backref='patient', lazy=True)
+
+
+class PatientService(database.Model):
+    __tablename__ = 'user_services'
+    id = database.Column(database.Integer, autoincrement=True, primary_key=True)
+    provided_service = database.Column(database.String(255), nullable=False)
+    service_date = database.Column(database.Date, nullable=False)
+    service_name = database.Column(database.String(100), nullable=False)
+    service_type = database.Column(database.String(255), nullable=False)
+    provider_name = database.Column(database.String(100), nullable=False)
+    provider_source = database.Column(database.String(150), nullable=False)
+    cost = database.Column(database.DECIMAL, nullable=False)
+    patient_id = database.Column(database.Integer, database.ForeignKey('patient.id'), nullable=False)
